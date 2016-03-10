13694863927cb8d184a06d75dd5b819c5a0fdc6b
Matt Messmer <staind2523@gmail.com>
2016-03-09 18:59:54 -0800

    Add repeating enemy shot.

---
+++ Assets/BulletController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(-5, 0);
+    }
+
+    private void OnCollisionEnter2D(Collision2D coll)
+    {
+        Debug.Log("Game over - player was hit.");
+        Destroy(gameObject);
+    }
+
+    private Rigidbody2D rb;
+}
---
+++ Assets/EnemyController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public GameObject Player;
+
+    private void Start()
+    {
+        if (Player == null)
+        {
+            throw new InvalidOperationException("Player must be set in editor.");
+        }
+
+        bullet = (GameObject)Resources.Load("Bullet");
+
+        // Shoot at player every second
+        InvokeRepeating("Shoot", 0, 1);
+    }
+
+    private void Shoot()
+    {
+        Instantiate(bullet, new Vector3(transform.position.x - 1, 1.5f), bullet.transform.rotation);
+    }
+
+    private GameObject bullet;
+}
