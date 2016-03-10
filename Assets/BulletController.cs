25a3e29922258861050a735c4524cb787bded109
Matt Messmer <staind2523@gmail.com>
2016-03-09 19:50:24 -0800

    Make enemy move toward player.

--- Assets/BulletController.cs
+++ Assets/BulletController.cs
@@ -5,7 +5,7 @@ public class BulletController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        rb.velocity = new Vector2(-5, 0);
+        rb.velocity = new Vector2(-10, 0);
     }
 
     private void OnCollisionEnter2D(Collision2D coll)
--- Assets/EnemyController.cs
+++ Assets/EnemyController.cs
@@ -12,10 +12,17 @@ public class EnemyController : MonoBehaviour
             throw new InvalidOperationException("Player must be set in editor.");
         }
 
+        rb = GetComponent<Rigidbody2D>();
+
         bullet = (GameObject)Resources.Load("Bullet");
 
         // Shoot at player every second
         InvokeRepeating("Shoot", 0, 1);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(-1, rb.velocity.y);
     }
 
     private void Shoot()
@@ -23,5 +30,6 @@ public class EnemyController : MonoBehaviour
         Instantiate(bullet, new Vector3(transform.position.x - 1, 1.5f), bullet.transform.rotation);
     }
 
+    private Rigidbody2D rb;
     private GameObject bullet;
 }
