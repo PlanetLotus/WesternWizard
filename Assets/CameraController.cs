b1d70754b80bcd65b0ec7e15580b9333fcce11b4
Matt Messmer <staind2523@gmail.com>
2016-03-12 13:36:06 -0800

    Clamp camera to size of level.
    If the viewport is larger than the size of the level, the camera
    effectively zooms in.

---
+++ Assets/CameraController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+	public Vector2 MaxXAndY;
+	public Vector2 MinXAndY;
+
+    private void FixedUpdate()
+    {
+		float targetX = Mathf.Clamp(transform.position.x, MinXAndY.x, MaxXAndY.x);
+		float targetY = Mathf.Clamp(transform.position.y, MinXAndY.y, MaxXAndY.y);
+
+        transform.position = new Vector3(targetX, targetY, transform.position.z);
+    }
+}
